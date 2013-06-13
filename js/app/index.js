2c83edf52a171d8d7df75406e1668df67aace1c9
Diwank Singh <diwank.singh@gmail.com>
2013-06-13 14:57:21 -0700

    Updated index.
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- js/app/index.js
+++ js/app/index.js
@@ -12,13 +12,13 @@ define(['app/namespace', 'thorax', 'modernizr'], function(Application, Thorax) {
     return really;
   }
 
-  // Allows load:end and load:start events to propagate
-  // to the application object
+  // Allows load:end and load:start events to propagate.
+  // to the application object.
   Thorax.setRootObject(Application);
 
   // Hell yeah!
   init = function init() {
-    // Reject incompatible devices
+    // Reject incompatible devices.
     if(!compatible()) {
       alert('Oops! Seems like your device isn\'t compatible..');
       return false;
@@ -27,18 +27,38 @@ define(['app/namespace', 'thorax', 'modernizr'], function(Application, Thorax) {
     var $, Backbone;
 
     // Load libs.
+    $ = Application.libs.$;
     Backbone = Application.libs.Backbone;
 
+    // Start Backbone history.
     Backbone.history.start({
-      pushState: false,
+      pushState: true,
       root: '/',
       silent: true
     });
 
+    // Render template.
     // Application.template = Thorax.templates.application;
     // Application.appendTo('body');
 
+    // Dispatch.
     Backbone.history.loadUrl();
+
+    // Setup a[href] pushstate listeners
+    $(document).on('click', 'a:not([data-bypass])', function(e) {
+      var href, protocol;
+
+      // Get the anchor href and protocol.
+      href = $(this).prop('href');
+      protocol = this.protocol;
+
+      // Ensure relative url.
+      if(href && href.slice(0, protocol.length) !== protocol) {
+        e.preventDefault();
+
+        // Application.router.navigate(href, {trigger: true});
+      }
+    });
   }
 
   // Exports
